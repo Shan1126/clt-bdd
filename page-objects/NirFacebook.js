8fc6d3dea6c8a78f4b690f665f0b2b455f72e8e4
Nirmala <snirmala2411@gmail.com>
2022-01-26 16:13:49 -0600

    Nirmala Facebook

--- page-objects/NirFacebook.js
+++ page-objects/NirFacebook.js
@@ -3,20 +3,18 @@ const helpers = require("../runtime/helpers");
 /* eslint-disable no-undef */
 module.exports = {
 
-    url: 'http://www.jootza.com',
+    url: 'http://www.facebook.com',
 
     elements: {
-        Login: '//*[@id="bs-example-navbar-collapse-1"]/ul/li[2]/a',
-        LoginHeader: '/html/body/mdb-root/main/div/app-login/header/section/div/div/div/div/div/div/div[1]/h2/strong',
-        username: by.name('username'),
-        password: by.name('password'),
-        LoginBtn: '//*[@id="btn-login"]/button',
-        errorMessage: '//*[@id="toast-container"]/mdb-toast-component/div'
+        username: by.name('email'),
+        password: by.name('pass'),
+        LoginBtn: '/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button',
+        errorMessage: '//*[@id="email_container"]/div[2]'
     },
     clickElement: async function(objectKey) {
         // eslint-disable-next-line no-console
         //LoginBtn
-        var selector = page.jootza.elements[objectKey];
+        var selector = page.NirFacebook.elements[objectKey];
 
         await driver.sleep(2000);
         return driver.findElement(By.xpath(selector)).click();
@@ -24,7 +22,7 @@ module.exports = {
     inputUserName: async function(val) {
         // val --> johndoe
         // objectKey --> input field
-        var selector = page.jootza.elements['username']; // //*[@id="login-username relate-pos"]
+        var selector = page.NirFacebook.elements['username']; // //*[@id="login-username relate-pos"]
         await driver.sleep(2000); // in millseconds 1000ms = 1 second
         return driver.findElement(selector).sendKeys(val);
 
@@ -32,14 +30,14 @@ module.exports = {
     inputPassword: async function(val) {
         // val --> johndoe
         // objectKey --> input field
-        var selector = page.jootza.elements['password']; // //*[@id="login-username relate-pos"]
+        var selector = page.NirFacebook.elements['password']; // //*[@id="login-username relate-pos"]
         await driver.sleep(2000); // in millseconds 1000ms = 1 second
         return driver.findElement(selector).sendKeys(val);
 
     },
     elementExists: async function(objectKey) {
         // eslint-disable-next-line no-console
-        var selector = page.jootza.elements[objectKey];
+        var selector = page.NirFacebook.elements[objectKey];
 
         await driver.sleep(5000);
         return driver.findElement(By.xpath(selector));
