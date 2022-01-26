ca86736ee1e37274db72352e8198b6c31f6482c0
Shan Gnanasambandham <sgnanasambandham.ctr@tistatech.com>
2022-01-25 21:13:29 -0500

    Updates

--- step-definitions/jootza-steps.js
+++ step-definitions/jootza-steps.js
@@ -8,6 +8,13 @@ module.exports = function () {
         return page.jootza.clickElement(objectKey);
     });
 
+    this.When(/^I enter username "([^"]*)"$/ , function(objKey1) {
+        return page.jootza.inputUserName(objKey1);
+    });
+
+    this.When(/^I enter password "([^"]*)"$/ , function(objKey1) {
+        return page.jootza.inputPassword(objKey1);
+    });
     this.Then(/^I should see "([^"]*)"$/, function (objectKey) {
         return page.jootza.elementExists(objectKey);
     });
