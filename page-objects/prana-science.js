96671c61c910ae4f73e5a25305527e771fd6134c
Lakshmipriya Gopal <mailtolakshmi13@gmail.com>
2022-01-30 06:17:25 -0500

    adding three changes

--- page-objects/prana-science.js
+++ page-objects/prana-science.js
@@ -3,20 +3,21 @@ const helpers = require("../runtime/helpers");
 /* eslint-disable no-undef */
 module.exports = {
 
-    url: 'https://www.pranascience.org',
+    url: 'https://www.pranascience.org/login',
 
     elements: { 
-        Signin: '//*[@id="topnav"]/div/div[2]/a',
+        
+        signup: '//*[@id="topnav"]/div/div[2]/a',
         LoginHeader1: '/html/body/app-root/app-auth-login/section'
     },
     clickElement: function(objectKey) {
         // eslint-disable-next-line no-console
-        var selector = page.pranascience.elements[objectKey];
+        var selector = page.pranaScience.elements[objectKey];
         return driver.findElement(By.xpath(selector)).click();
     },
     elementExists: function(objectKey) {
         // eslint-disable-next-line no-console
-        var selector = page.pranascience.elements[objectKey];
+        var selector = page.pranaScience.elements[objectKey];
         return driver.findElement(By.xpath(selector));
     }
 };
--- step-definitions/prana-science-steps.js
+++ step-definitions/prana-science-steps.js
@@ -1,17 +1,17 @@
 module.exports = function () {
 
     this.Given('I am on the Pranascience portal',  function () {
-        return helpers.loadPage('http://www.pranascience.org');
+        return helpers.loadPage('http://www.pranascience.org/login');
     });
    
 
     this.When(/^I click on "([^"]*)" link$/,  function (objectKey) {
-            return page.pranascience.clickElement(objectKey);
+            return page.pranaScience.clickElement(objectKey);
     });
 
 
     this.Then(/^I should see "([^"]*)" screen$/,  function (objectKey) {
-        return page.pranascience.elementExists(objectKey);
+        return page.pranaScience.elementExists(objectKey);
     });
 
 
