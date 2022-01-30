519e77890cba663fbd1dbdff346050f735a5745f
Mani <kmani@gategroup.com>
2022-01-30 14:52:19 -0600

    Adding modified pranascience by Raji

--- page-objects/rprana.js
+++ page-objects/rprana.js
@@ -22,24 +22,22 @@ module.exports = {
         EmailExists: '/html/body/app-root/app-auth-signup/section/div/div/div[1]/div/div/form/div/div[10]/div'
     },
 
-  clickElement: async function (objectKey) {
-       
+    clickElement: async function (objectKey) {
+
         var selector = page.rprana.elements[objectKey];
         await driver.sleep(2000);
         var element = driver.findElement(By.xpath(selector));
         return driver.executeScript('return arguments[0].click();', element);
     },
 
-    inputTextBoxData: async function(textboxElementName, value)
-    {
+    inputTextBoxData: async function (textboxElementName, value) {
         var selector = page.rprana.elements[textboxElementName];
         await driver.sleep(2000); // in millseconds 1000ms = 1 second
         return driver.findElement(By.xpath(selector)).sendKeys(value);
     },
 
 
-    selectDropDown: async function (selectName, value)
-    {
+    selectDropDown: async function (selectName, value) {
         var selector = page.rprana.elements[selectName];
         await driver.sleep(2000); // in millseconds 1000ms = 1 second
         var element = driver.findElement(By.xpath(selector));
